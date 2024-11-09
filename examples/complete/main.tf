b2c523740b34dedbbcd3daff8c0414e844210a48
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-11-09 12:09:53 +0000

    chore(deps): update terraform terraform-ibm-modules/secrets-manager-public-cert-engine/ibm to v1.0.2 (#121)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -66,7 +66,7 @@ locals {
 module "secrets_manager_public_cert_engine" {
   count   = var.existing_sm_instance_guid == null ? 1 : 0
   source  = "terraform-ibm-modules/secrets-manager-public-cert-engine/ibm"
-  version = "1.0.1"
+  version = "1.0.2"
   providers = {
     ibm              = ibm
     ibm.secret-store = ibm.secret-store
