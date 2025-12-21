7e4f418f7941238114754a8f874e841542a93a15
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-21 18:20:21 +0000

    fix(deps): update terraform terraform-ibm-modules/secrets-manager-public-cert-engine/ibm to v1.6.6 (#295)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -68,7 +68,7 @@ locals {
 module "secrets_manager_public_cert_engine" {
   count   = var.existing_sm_instance_guid == null ? 1 : 0
   source  = "terraform-ibm-modules/secrets-manager-public-cert-engine/ibm"
-  version = "1.6.5"
+  version = "1.6.6"
   providers = {
     ibm              = ibm
     ibm.secret-store = ibm.secret-store
