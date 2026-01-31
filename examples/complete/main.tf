87de4a06044dfde6fd43281dd0246c87a691cde5
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-31 00:46:10 +0000

    chore(deps): update dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -36,7 +36,7 @@ module "resource_group" {
 module "secrets_manager" {
   count                = var.existing_sm_instance_guid == null ? 1 : 0
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.12.25"
+  version              = "2.13.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = local.sm_region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
@@ -49,7 +49,7 @@ module "secrets_manager" {
 # Best practise, use the secrets manager secret group module to create a secret group
 module "secrets_manager_secret_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.3.39"
+  version                  = "1.4.1"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-certificates-secret-group"   #checkov:skip=CKV_SECRET_6: does not require high entropy string as is static value
@@ -68,7 +68,7 @@ locals {
 module "secrets_manager_public_cert_engine" {
   count   = var.existing_sm_instance_guid == null ? 1 : 0
   source  = "terraform-ibm-modules/secrets-manager-public-cert-engine/ibm"
-  version = "1.6.18"
+  version = "1.6.19"
   providers = {
     ibm              = ibm
     ibm.secret-store = ibm.secret-store
